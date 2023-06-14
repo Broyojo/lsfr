5519e943f1cbd6108e34347390f25c2ccb22a20c
Broyojo <dha@xoba.com>
2023-06-14 10:33:02 -0400

    got basic training working

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
 from dataclasses import dataclass
 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 import torch
 import torch.nn.functional as F
-from torch import nn
+from PIL import Image
+from torch import nn, optim
+from torchvision.transforms import ToTensor
+from tqdm import tqdm
 
 
 @dataclass
@@ -106,9 +113,14 @@ class VisionTransformer(nn.Module):
             linear_layer = nn.Linear(config.n_embed, config.codebook_size, bias=False),
             dropout = nn.Dropout(config.dropout),
         ))
+
+        self.mask_embed = nn.Embedding(1, config.n_embed)
     
-    def forward(self, x):
+    def forward(self, x, mask=None):
         x = self.layers.patch_embed(x)
+        if mask is not None:
+            # remove all target mask embeddings and will those places with mask embedding
+            x = x * ~mask.unsqueeze(-1) + self.mask_embed.weight * mask.unsqueeze(-1)
         pos = torch.arange(0, x.shape[1], dtype=torch.long).unsqueeze(0)
         x = x + self.layers.pos_embed(pos)
         x = self.layers.dropout(x)
@@ -132,11 +144,10 @@ if __name__ == "__main__":
         patch_size=25,
     )
 
-    import os
+    model = VisionTransformer(config)
 
-    from PIL import Image
-    from torchvision.transforms import ToTensor
-    from tqdm import tqdm
+    print(model)
+    print(f"model has {sum(p.numel() for p in model.parameters()):,} parameters")
 
     images = []
 
@@ -147,27 +158,97 @@ if __name__ == "__main__":
                 image = Image.open(file_path)
                 images.append(image)
 
-    images = images[:32]
+    n_images = len(images)
+    indices = np.random.permutation(n_images)
 
+    train_size = int(n_images * 0.9)
+    train_indices, val_indices = indices[:train_size], indices[train_size:]
+
+    train_images = [images[i] for i in train_indices]
+    val_images = [images[i] for i in val_indices]
+
+    batch_size = 32
+    mask_prob = 0.2
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters())
     transform = ToTensor()
-    images = [transform(image).to("cuda") for image in images]
+    train_losses = []
+    val_losses = []
 
-    batch = torch.stack(images, dim=0)
+    print("steps:", len(train_images) // batch_size)
 
-    model = VisionTransformer(config)
-    print(model)
+    for i in range(0, len(train_images)-batch_size, batch_size):
+        optimizer.zero_grad()
 
-    for _ in tqdm(range(1)):
-        output = model(batch)
+        train_batch_images = [transform(image).to("cuda") for image in train_images[i : i + batch_size]]
+        train_batch = torch.stack(train_batch_images, dim=0)
 
-    print(output)
-    print(output.shape)
+        no_mask_output = model(train_batch)
 
-    import matplotlib.pyplot as plt
-    import seaborn as sns
+        random_tensor = torch.rand(batch_size, config.seq_length)
+        mask = random_tensor < mask_prob
+        mask_output = model(train_batch, mask)
 
-    plt.figure(figsize=(10, 10))
-    sns.heatmap(output.detach().cpu().numpy()[0], cmap="YlGnBu")
-    plt.ylabel("each image patch")
-    plt.xlabel("probability distribution over codebook tokens")
+        train_loss = loss_fn(
+            mask_output.view(-1, mask_output.size(-1)),
+            torch.argmax(no_mask_output, dim=-1).view(-1)
+        ) + loss_fn(
+            no_mask_output.view(-1, no_mask_output.size(-1)),
+            torch.argmax(mask_output, dim=-1).view(-1)
+        )
+
+        print(f"Train loss for batch {i // batch_size}: {train_loss.item()}")
+        train_losses.append(train_loss.item())
+        train_loss.backward()
+        optimizer.step()
+
+        # Validate every 10 samples
+        if i % (10 * batch_size) == 0:
+            with torch.no_grad():
+                val_batch_images = [transform(image).to("cuda") for image in val_images[:batch_size]] # Take first batch from validation images
+                val_batch = torch.stack(val_batch_images, dim=0)
+
+                no_mask_output_val = model(val_batch)
+
+                random_tensor_val = torch.rand(batch_size, config.seq_length)
+                mask_val = random_tensor_val < mask_prob
+                mask_output_val = model(val_batch, mask_val)
+
+                val_loss = loss_fn(
+                    mask_output_val.view(-1, mask_output_val.size(-1)),
+                    torch.argmax(no_mask_output_val, dim=-1).view(-1)
+                ) + loss_fn(
+                    no_mask_output_val.view(-1, no_mask_output_val.size(-1)),
+                    torch.argmax(mask_output_val, dim=-1).view(-1)
+                )
+
+                print(f"Validation loss: {val_loss.item()}")
+                val_losses.append(val_loss.item())
+
+    plt.plot(train_losses, label='Training loss')
+    plt.plot(np.arange(0, len(train_losses), step=10), val_losses, label='Validation loss')
+    plt.yscale("log")
+    plt.legend()
     plt.show()
+
+    torch.save(model.state_dict(), 'mim_transformer.pt')
+
+
+    # for _ in tqdm(range(1)):
+    #     random_tensor = torch.rand(batch_size, config.seq_length)
+    #     mask = random_tensor < mask_prob
+    #     print(mask)
+    #     output = model(batch, mask)
+
+    # print(output)
+    # print(output.shape)
+
+    # import matplotlib.pyplot as plt
+    # import seaborn as sns
+
+    # plt.figure(figsize=(10, 10))
+    # sns.heatmap(mask.detach().cpu().numpy(), cmap="YlGnBu")
+    # plt.ylabel("each image patch")
+    # plt.xlabel("probability distribution over codebook tokens")
+    # plt.show()
